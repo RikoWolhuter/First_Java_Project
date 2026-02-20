a39c8c8a93034c221c23f55dde9285c5884e56ec
Riko Wolhuter <47495097+RikoWolhuter@users.noreply.github.com>
2026-02-20 15:17:44 +0200

    Some basic java code

--- GithubProject/src/githubproject/GithubProject.java
+++ GithubProject/src/githubproject/GithubProject.java
@@ -2,43 +2,15 @@
 package githubproject;
 
 
-public class GithubProject {
-    /*This is a 
-    multi-line
-    comment
-    */
-    
-    //What is a string
-    //String is array of characters
-    //john
-    //[j][o][h][n]
-    //[x0001][x0002]...etc
-    public static void main(String[] args) {
-        
-        boolean Truth = true;
-        int myNum = 9;
-        //int Sum = Truth + myNum;
-        
-        //Intention - Print my name in Capitalized letters
-        System.out.println("JHONATHAn");
-        
-       //
-       System.out.println("My name is John");
-       System.out.print("I am a funny");
-       //This is an invoice
-       //ID     |Age    |Name   |Gender
-       //41414  |13     |John   |Male
 
+public class GithubProject {
+    
+    
+    
+    public static void main(String[] args) {
+        System.out.println("Hello there!");
     }
    
 }
 
 
-/*
-I am working on the Pacman characters
-Colour variable is to detail the colour of the eyes
-ColourShape variable is to colour the shape of pacman
-*/
-
-
-//TODO later - on line 118 there is a logic error where library Scanner is not imported
